a1189f23f61472fd530f44c483a0099f47e31d24
jgam <jgam@alumni.nd.edu>
2021-01-05 14:23:26 +0900

    [FEAT] api done

--- jgam-project-cli3/src/views/NewsView.vue
+++ jgam-project-cli3/src/views/NewsView.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
       news
+      <div v-for="(user, index) in users" v-bind:key ="index">{{user}}</div>
   </div>
 </template>
 
 <script>
-export default {
+import {fetchList} from '../api/index';
 
+export default {
+    data() {
+        return{
+            users: []
+        }
+    },
+    created(){
+        fetchList('news').then(response => this.users = response.data).catch(function(error){console.log(error)});
+    }
 }
 </script>
 
