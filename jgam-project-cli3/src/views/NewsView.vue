b6084bfe18b10a18cbbee6b1d12dfb102177acd2
jgam <jgam@alumni.nd.edu>
2021-01-06 10:17:49 +0900

    [FEAT] dynamic routes

--- jgam-project-cli3/src/views/NewsView.vue
+++ jgam-project-cli3/src/views/NewsView.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
       news
-      <div class="news-list" v-for="(user, index) in this.news" v-bind:key ="index">
+      <div class="news-list" v-for="(user, index) in this.$store.state.news" v-bind:key ="index">
           <div class="title">{{user.title}}</div>
           <div class="info">
             <div>comments: {{user.comments_count}}</div>
             <a v-bind:href="user.url">go to News</a>
           </div>
-            <div>{{user.time_ago}} by {{user.user}}</div>
+            <div>{{user.time_ago}} by <router-link v-bind:to="`/news/${user.user}`">{{user.user}}</router-link></div>
           
       </div>
   </div>
