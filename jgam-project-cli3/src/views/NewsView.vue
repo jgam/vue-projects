fb1f215e343aeb5e550f197967442d3985d121cc
jgam <jgam@alumni.nd.edu>
2021-01-06 09:33:57 +0900

    [STYLE] news view

--- jgam-project-cli3/src/views/NewsView.vue
+++ jgam-project-cli3/src/views/NewsView.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
       news
-      <div v-for="(user, index) in this.news" v-bind:key ="index">{{user}}</div>
+      <div class="news-list" v-for="(user, index) in this.news" v-bind:key ="index">
+          <div class="title">{{user.title}}</div>
+          <div class="info">
+            <div>comments: {{user.comments_count}}</div>
+            <a v-bind:href="user.url">go to News</a>
+          </div>
+            <div>{{user.time_ago}} by {{user.user}}</div>
+          
+      </div>
   </div>
 </template>
 
@@ -19,6 +27,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.news-list{
+    padding: 10px;
+}
+.news-list .title{
+    font-size: 20px;
+}
 
-</style>
+.news-list .info{
+    display: flex;
+    justify-content: space-between;
+}
+
+</style>>
